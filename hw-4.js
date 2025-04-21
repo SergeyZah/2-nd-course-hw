be0f5dfd0b43a1c679577755762e4241b20ddcb2
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-04-21 17:17:54 +0300

    Made task 4.2

--- hw-4.js
+++ hw-4.js
@@ -10,4 +10,16 @@ function min(a, b) {
 
 console.log(min(40, 10));
 
+// Задание 2
+
+function isEven(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+
+console.log(isEven(3));
+
 
