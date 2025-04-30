ebdb6a86a231e1924f3ad99bb5b9fd41789d2e83
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-04-30 20:29:42 +0300

    Made task 5.3

--- hw-5.js
+++ hw-5.js
@@ -9,11 +9,18 @@
 
 // Задание 2
 
-const numbers = [1, 5, 4, 10, 0, 3];
+// const numbers = [1, 5, 4, 10, 0, 3];
 
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === 4) {
-        console.log(`Число 4 под индексом: ${i}`)
-    };
-};
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === 4) {
+//         console.log(`Число 4 под индексом: ${i}`)
+//     };
+// };
 
+
+// Задание 3
+
+const numbs = '1, 3, 5, 10, 20';
+
+let joinNumbs = numbs.split(", ").join(' ');
+console.log(joinNumbs);
