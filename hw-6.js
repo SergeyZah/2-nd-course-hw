df3f6c5d42964137eac91371776c7ee5a6a578a2
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-05-04 15:38:51 +0300

    Made the task 6.2

--- hw-6.js
+++ hw-6.js
@@ -6,3 +6,21 @@ console.log(string);
 console.log(string.toUpperCase());
 
 
+// Задание 2
+
+function searchString(array, string) {
+
+    array.forEach((product) => {
+        if (product.toLowerCase().startsWith(string.toLowerCase())) {
+            console.log(product);
+        }
+    })
+}
+
+const a = ['Молоко пастеризованное', 'Хлеб бородинский', 'Сыр классический', 'Молоко безлактозное', 'Колбаса вареная', 'Молоко топленое', 'Молоко парное'];
+const b = 'молоко';
+
+console.log(a);
+searchString(a, b);
+
+
