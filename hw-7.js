77b057cabee3e543476037e4a7b8451364cd9a3a
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-05-06 17:35:09 +0300

    Made the task 7.5

--- hw-7.js
+++ hw-7.js
@@ -73,12 +73,37 @@
 
 // Задание 4
 
-function delayForSecond(callback) {
+// function delayForSecond(callback) {
+//     setTimeout(() => {
+//         callback();
+//     }, 1000);
+// }
+
+// delayForSecond(function () {
+//     console.log('Привет, Глеб!');
+// })
+
+
+// Задание 5
+
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
     setTimeout(() => {
-        callback();
-    }, 1000);
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
 }
 
-delayForSecond(function () {
-    console.log('Привет, Глеб!');
-})
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond();
+setTimeout(() => {
+    sayHi('Глеб')
+}, 2000);
