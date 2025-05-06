62c3f48396e7ac0de088fa4c84ac8a3bd217a55e
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-05-06 16:49:43 +0300

    Made the task 7.2

--- hw-7.js
+++ hw-7.js
@@ -1,14 +1,54 @@
 // Задание 1
 
+// const people = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+// ];
+
+// console.log(people.sort((a, b) => a.age - b.age));
+
+
+// Задание 2
+
+function isPositive(a) {
+
+    if (a > 0) {
+        return a;
+    } else {
+        return;
+    }
+}
+
+function isMale(a) {
+
+    if (a.gender === 'male') {
+        return a;
+    }
+}
+
+function filter(arr, ruleFunction) {
+
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (ruleFunction(arr[i])) {
+            output.push(ruleFunction(arr[i]));
+        }
+    }
+    return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
 const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
 ];
 
-console.log(people.sort((a, b) => a.age - b.age));
-
-
-
+console.log(filter(people, isMale));
 
