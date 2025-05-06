c43e4471890c65dc2aea1cb490fd0975bb98718a
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-05-06 17:05:51 +0300

    Made the task 7.3

--- hw-7.js
+++ hw-7.js
@@ -12,43 +12,62 @@
 
 // Задание 2
 
-function isPositive(a) {
+// function isPositive(a) {
 
-    if (a > 0) {
-        return a;
-    } else {
-        return;
-    }
-}
+//     if (a > 0) {
+//         return a;
+//     } else {
+//         return;
+//     }
+// }
 
-function isMale(a) {
+// function isMale(a) {
 
-    if (a.gender === 'male') {
-        return a;
-    }
-}
+//     if (a.gender === 'male') {
+//         return a;
+//     }
+// }
 
-function filter(arr, ruleFunction) {
+// function filter(arr, ruleFunction) {
 
-    const output = [];
+//     const output = [];
 
-    for (let i = 0; i < arr.length; i++) {
+//     for (let i = 0; i < arr.length; i++) {
 
-        if (ruleFunction(arr[i])) {
-            output.push(ruleFunction(arr[i]));
-        }
-    }
-    return output;
-}
+//         if (ruleFunction(arr[i])) {
+//             output.push(ruleFunction(arr[i]));
+//         }
+//     }
+//     return output;
+// }
 
-console.log(filter([3, -4, 1, 9], isPositive));
+// console.log(filter([3, -4, 1, 9], isPositive));
 
-const people = [
-    { name: 'Глеб', gender: 'male' },
-    { name: 'Анна', gender: 'female' },
-    { name: 'Олег', gender: 'male' },
-    { name: 'Оксана', gender: 'female' }
-];
+// const people = [
+//     { name: 'Глеб', gender: 'male' },
+//     { name: 'Анна', gender: 'female' },
+//     { name: 'Олег', gender: 'male' },
+//     { name: 'Оксана', gender: 'female' }
+// ];
 
-console.log(filter(people, isMale));
+// console.log(filter(people, isMale));
+
+
+// Задание 3
+
+const timer = (deadline) => {
+    const date = new Date();
+    console.log(date.toLocaleDateString('ru-RU'));
+    const interval = setInterval(() => {
+        console.log(date.toLocaleDateString('ru-RU'));
+    }, 3000);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log(`${deadline} секунд прошло`)
+    }, deadline * 1000)
+};
+
+timer(30);
+
 
